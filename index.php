404e8f5b29e4bdd78ab241afb59f0cb6cb8d0886
Everaldo Scapin <everaldo.scp@gmail.com>
2023-04-26 17:34:41 -0300

    Criado a classe Page

--- index.php
+++ index.php
@@ -2,17 +2,18 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$Sql = new Hcode\DB\Sql();
+    $page = new Page();
 
-	$results = $Sql->select("SELECT * FROM tb_users");
-
-	echo json_encode($results);
+    $page->setTpl("index");
 
 });
 
